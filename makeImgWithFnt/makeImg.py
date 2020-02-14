8961ab5048a2d7fedd5c5e23d0b6a3430b7f7742
zhengshanzhou <zzs2260736@126.com>
2020-02-14 16:16:23 +0800

    [dev] 图片属性更改

--- makeImgWithFnt/makeImg.py
+++ makeImgWithFnt/makeImg.py
@@ -1,18 +1,25 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 # __author__ = "Damon"
-# Date: 2020/2/13
+# Date: 2020/2/14
 
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageEnhance
 
-font_size = 7
-text = "泓宇小可爱！"
+font_size = 8
+text = "小可爱！"
 img_path = "E://testMakeImg/1.jpg"
+img_data = Image.open(img_path)
+img_data = ImageEnhance.Color(img_data).enhance(1.2)
+img_data = ImageEnhance.Brightness(img_data).enhance(1.2)
+img_data = ImageEnhance.Contrast(img_data).enhance(1.2)
+img_data = ImageEnhance.Sharpness(img_data).enhance(1.1)
 
-img_raw = Image.open(img_path)
-img_array = img_raw.load()
+img_data.save("E://testMakeImg/3.jpg")
 
-img_new = Image.new("RGB", img_raw.size, (0, 0, 0))
+img_data = Image.open("E://testMakeImg/3.jpg")
+img_array = img_data.load()
+
+img_new = Image.new("RGB", img_data.size, (80, 80, 80))
 draw = ImageDraw.Draw(img_new)
 font = ImageFont.truetype('C:/Windows/Fonts/微软雅黑/msyhl.ttc', font_size)
 
@@ -25,8 +32,8 @@ def character_generator(text):
 
 ch_gen = character_generator(text)
 
-for y in range(0, img_raw.size[1], font_size):
-    for x in range(0, img_raw.size[0], font_size):
+for y in range(0, img_data.size[1], font_size):
+    for x in range(0, img_data.size[0], font_size):
         draw.text((x, y), next(ch_gen), font=font, fill=img_array[x, y], direction=None)
 
 img_new.convert('RGB').save("E://testMakeImg/2.jpg")
